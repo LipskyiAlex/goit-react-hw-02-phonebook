8323339e647e29bd72a717b616919bc24bc521f2
Alex <alexlipskyi@icloud.com>
2023-09-10 14:12:13 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from "react";
+import {Input} from './input/index';
+
+
+class App extends React.Component {
+
+  state = {
+
+    contacts: [],
+    name: ''
+    
+  }
+
+
+  render() {
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        React homework template
+
+        <Input />
+      </div>
+    );
+
+  }
+ 
 };
+
+
+export default App;
