539dfc0b38abef5b7faf35383680295ab2c673d5
Alex <alexlipskyi@icloud.com>
2023-09-15 12:40:38 +0300

    done

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,32 +1,37 @@
-import {AiFillDelete} from "react-icons/ai";
-import {FcBusinessContact} from "react-icons/fc";
-import {BsTelephoneOutbound} from 'react-icons/bs';
-import {List,Item,EmptyList,Contact,Number,Wrapper} from './contacts.styled';
+import { AiFillDelete } from 'react-icons/ai';
+import { FcBusinessContact } from 'react-icons/fc';
+import { BsTelephoneOutbound } from 'react-icons/bs';
+import {
+  List,
+  Item,
+  EmptyList,
+  Contact,
+  Number,
+  Wrapper,
+} from './contacts.styled';
 
-const Contacts = ({ contacts, filter, onDelete }) => {
+const Contacts = ({ contacts, onDelete }) => {
   return (
     <>
       <List>
-        {contacts.length===0 && (<EmptyList>There aren't contacts yet. Let's add somebody.</EmptyList>)}
-        {contacts
-          .filter(({ contact }) =>
-            contact.toLowerCase().includes(filter.toLowerCase())
-          )
-          .map(({ id, contact, number }) => (
-            <Item key={id}>
-              <Wrapper>
+        {contacts.length === 0 && (
+          <EmptyList>There aren't contacts yet. Let's add somebody.</EmptyList>
+        )}
+        {contacts.map(({ id, contact, number }) => (
+          <Item key={id}>
+            <Wrapper>
               <FcBusinessContact size={20} />
               <Contact> {contact}</Contact>
-              </Wrapper>
-              <Wrapper>
+            </Wrapper>
+            <Wrapper>
               <BsTelephoneOutbound size={20} />
               <Number>{number}</Number>
-              </Wrapper>
-              <div role="button" aria-label="Delete" onClick={() => onDelete(id)}>
+            </Wrapper>
+            <div role="button" aria-label="Delete" onClick={() => onDelete(id)}>
               <AiFillDelete size={20} />
-              </div>
-            </Item>
-          ))}
+            </div>
+          </Item>
+        ))}
       </List>
     </>
   );
