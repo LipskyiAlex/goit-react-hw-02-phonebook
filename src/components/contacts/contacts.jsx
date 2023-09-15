8fe1fbfba59a98ca647c613147abb1df728e53a5
Alex <alexlipskyi@icloud.com>
2023-09-15 09:50:19 +0300

    before buton

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,20 +1,29 @@
 import {AiFillDelete} from "react-icons/ai";
-import {List,Item,Button} from './contacts.styled';
+import {FcContacts} from "react-icons/fc";
+import {BsTelephoneOutbound} from 'react-icons/bs';
+import {List,Item,Button,EmptyList,Contact,Number,Wrapper} from './contacts.styled';
 
 const Contacts = ({ contacts, filter, onDelete }) => {
   return (
     <>
       <List>
+        {contacts.length===0 && (<EmptyList>There aren't contacts yet. Let's add somebody.</EmptyList>)}
         {contacts
           .filter(({ contact }) =>
             contact.toLowerCase().includes(filter.toLowerCase())
           )
           .map(({ id, contact, number }) => (
             <Item key={id}>
-              <p>{contact}</p>
-              <p>{number}</p>
+              <Wrapper>
+              <FcContacts size={20} />
+              <Contact> {contact}</Contact>
+              </Wrapper>
+              <Wrapper>
+              <BsTelephoneOutbound size={20} />
+              <Number>{number}</Number>
+              </Wrapper>
               <Button type="button" onClick={() => onDelete(id)}>
-              <AiFillDelete size={12} />
+              <AiFillDelete size={20} />
               </Button>
             </Item>
           ))}
--- src/components/contacts/contacts.styled.jsx
+++ src/components/contacts/contacts.styled.jsx
@@ -4,20 +4,46 @@ import styled from "@emotion/styled";
 export const List = styled.ul`
 
 width:100%;
-
-
+padding-left:15px;
+padding-right:15px;
 
 `
 
 export const Item = styled.li`
 
 display:flex;
-justify-content:space-between;
-
+align-items:center;
+justify-content: space-between;
 `
 
 export const Button = styled.button`
 
-margin-right:10px;
+width:70px;
 
+display:flex;
+justify-content:center;
+
+`
+
+export const EmptyList = styled.p`
+
+ font-size:20px;
+ font-style: italic;
+
+
+`
+
+export const Contact = styled.p`
+
+font-size:20px;
+
+`
+
+export const Number = Contact.withComponent('number');
+
+export const Wrapper = styled.div`
+ 
+display:flex;
+gap:10px;
+align-items:center;
 `
