978620d5bedb0d0ac4f14bc4917a2174bc9363d5
Alex <alexlipskyi@icloud.com>
2023-09-15 12:09:08 +0300

    filter for

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,10 @@ import { MainContainer,MainTitle,SecondaryTitle } from './App.styled';
 class App extends React.Component {
   state = {
     contacts: [
-   
+      { id: 'id-1', contact: 'Rosie Simpson', number: '459-12-56' },  //! ОЧИСТИТЬ МАССИВ
+      { id: 'id-2', contact: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', contact: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', contact: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -23,7 +26,7 @@ class App extends React.Component {
 
   handleSubmit = (values, { resetForm }) => {
     const { name, number } = values;
-
+     const {contacts} = this.state;
     resetForm();
     const contactId = nanoid();
     const newContact = {
@@ -32,12 +35,11 @@ class App extends React.Component {
       number: number,
     };
 
-    for (let name of this.state.contacts) {
-      const { contact } = name;
-      if (newContact.contact.toLowerCase() === contact.toLowerCase()) {
-        return Notiflix.Notify.failure(`${contact} is already in contacts`);
-      }
+    
+    if(contacts.filter(({contact})  =>  contact.toLowerCase() === newContact.contact.toLowerCase()).length !== 0) {
+      return Notiflix.Notify.failure(`${newContact.contact} is already in contacts`);
     }
+    
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
